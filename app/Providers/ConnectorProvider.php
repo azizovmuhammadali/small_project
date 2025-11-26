b73edd1422c51b8fe1ead82bd209e7207e266ed1
azizovmuhammadali <alico.777777@gmail.com>
2025-11-26 20:10:55 +0500

    connected ticket interfaces and classes

--- app/Providers/ConnectorProvider.php
+++ app/Providers/ConnectorProvider.php
@@ -4,13 +4,17 @@ namespace App\Providers;
 
 use App\Services\UserService;
 use App\Services\ClientService;
+use App\Services\TicketService;
 use App\Reposities\UserReposity;
 use App\Reposities\ClientReposity;
+use App\Reposities\TicketReposity;
 use Illuminate\Support\ServiceProvider;
 use App\Interfaces\Services\UserServiceInterface;
 use App\Interfaces\Services\ClientServiceInterface;
+use App\Interfaces\Services\TicketServiceInterface;
 use App\Interfaces\Reposities\UserReposityInterface;
 use App\Interfaces\Reposities\ClientReposityInterface;
+use App\Interfaces\Reposities\TickentReposityInterface;
 
 class ConnectorProvider extends ServiceProvider
 {
@@ -23,6 +27,8 @@ class ConnectorProvider extends ServiceProvider
         $this->app->bind(UserReposityInterface::class,UserReposity::class);
         $this->app->bind(ClientServiceInterface::class,ClientService::class);
         $this->app->bind(ClientReposityInterface::class,ClientReposity::class);
+        $this->app->bind(TicketServiceInterface::class, TicketService::class);
+        $this->app->bind(TickentReposityInterface::class, TicketReposity::class);
     }
 
     /**
