ccbd21eec2bf4c5b8fb915890c46d6588fc80bd7
azizovmuhammadali <alico.777777@gmail.com>
2025-11-26 19:26:35 +0500

    added client route

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ClientController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 Route::post('user/register',[UserController::class,'register']);
+Route::post('client/register',[ClientController::class,'register']);
