ffee1e0c312bddcff822fdf0bbcbe9445e510763
azizovmuhammadali <alico.777777@gmail.com>
2025-11-26 20:11:09 +0500

    added ticket store and status update routes

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,12 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ClientController;
+use App\Http\Controllers\TicketController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-Route::post('user/register',[UserController::class,'register']);
-Route::post('client/register',[ClientController::class,'register']);
+Route::post('users/register',[UserController::class,'register']);
+Route::post('clients/register',[ClientController::class,'register']);
+Route::post('tickets/create',[TicketController::class,'create']);
+Route::patch('tickets/{id}/status',[TicketController::class,'statusUpdate']);
